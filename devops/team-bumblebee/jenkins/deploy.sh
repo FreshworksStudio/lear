ec276a643399cc656903b9e8eb79f45c5b21adf3
Gyanesh Mishra <gyan@freshworks.io>
2019-09-11 13:03:17 -0700

    Test Jenkinsfile

--- devops/team-bumblebee/jenkins/deploy.sh
+++ devops/team-bumblebee/jenkins/deploy.sh
@@ -4,7 +4,11 @@ export OCP_PIPELINE_CLI_VERSION='0.0.6'
 export OCP_PIPELINE_CLI_URL="https://raw.githubusercontent.com/BCDevOps/ocp-cd-pipeline/v${OCP_PIPELINE_CLI_VERSION}/src/main/resources/pipeline-cli"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s deploy --config=config.groovy --pr=4 --env=dev
 =======
 curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s deploy --config=config.groovy --pr=0 --env=dev
 >>>>>>> a9c9ff2... add jenkins build/deploy config
+=======
+curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s deploy --config=config.groovy --pr=4 --env=dev
+>>>>>>> dd6c02e... Test Jenkinsfile

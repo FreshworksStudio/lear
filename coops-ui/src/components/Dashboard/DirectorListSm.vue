8ba5978f384f7ec3b052f954b30cfba72ce06d02
Cameron Bowler <cameron@freshworks.io>
2019-09-27 14:33:00 -0700

    Updated Dashboard Directors UI

--- coops-ui/src/components/Dashboard/DirectorListSm.vue
+++ coops-ui/src/components/Dashboard/DirectorListSm.vue
@@ -1,47 +1,86 @@
 <template>
   <ul class="list">
-    <li class="list-item" v-for="director in directors" v-bind:key="director.id">
-      <v-avatar color="primary" size="25">
-        <span class="white--text small">{{ director.officer.firstName.substring(0,1)}}</span>
-      </v-avatar>
-      <div class="director-info">
-        <div class="list-item__title">{{ director.officer.firstName }} {{ director.officer.lastName }}</div>
-        <div class="list-item__subtitle">
-          <ul class="address-details">
-            <li>{{ director.deliveryAddress.streetAddress }}</li>
-            <li>{{ director.deliveryAddress.addressCity }} {{ director.deliveryAddress.addressRegion }}
-              &nbsp;&nbsp;{{ director.postalCode}}</li>
-            <li>{{ director.deliveryAddress.addressCountry }}</li>
-            </ul>
+    <v-expansion-panel :expand="true">
+      <v-expansion-panel-content v-for="director in directors" v-bind:key="director.id">
+        <div class="header" slot="header">
+          <v-avatar color="primary" size="25">
+            <span class="white--text small">{{ director.officer.firstName.substring(0,1)}}</span>
+          </v-avatar>
+          <div class="list-item__title">{{ director.officer.firstName }} {{ director.officer.lastName }}</div>
         </div>
-      </div>
-    </li>
+        <li class="list-item" >
+          <div class="director-info">
+            <div class="address-title">Delivery Address</div>
+            <div class="list-item__subtitle">
+              <ul class="address-details">
+                <li>{{ director.deliveryAddress.streetAddress }}</li>
+                <li>{{ director.deliveryAddress.addressCity }} {{ director.deliveryAddress.addressRegion }}
+                  &nbsp;&nbsp;{{ director.postalCode}}</li>
+                <li>{{ director.deliveryAddress.addressCountry }}</li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li class="list-item" v-if="entityFilter(EntityTypes.BCorp)">
+          <div class="director-info">
+            <div class="address-title">Mailing Address</div>
+            <div class="list-item__subtitle">
+              <ul class="address-details">
+                <li>{{ director.deliveryAddress.streetAddress }}</li>
+                <li>{{ director.deliveryAddress.addressCity }} {{ director.deliveryAddress.addressRegion }}
+                  &nbsp;&nbsp;{{ director.postalCode}}</li>
+                <li>{{ director.deliveryAddress.addressCountry }}</li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
   </ul>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { mixins } from 'vue-class-component'
 import { mapState } from 'vuex'
+import EntityFilter from '@/mixins/entityFilter-mixin'
+import { EntityTypes } from '@/enums'
 
 @Component({
   computed: {
     ...mapState(['directors'])
   }
 })
-export default class DirectorListSm extends Vue {
+export default class DirectorListSm extends mixins(EntityFilter) {
   readonly directors: Array<object>
+
+  // EntityTypes Enum
+  private EntityTypes: {} = EntityTypes
 }
 </script>
 
 <style lang="stylus" scoped>
-  .address-details
-    padding 0
-    list-style-type none
+
+  .header
+    display: flex
 
   .list-item
     flex-direction row
-    align-items center
     background #ffffff
+    border-top none
+    padding: .25rem 1.25rem
+
+  .director-info
+    line-height 1.325rem
+
+  .list-item__title
+    font-weight: 500
+
+  .address-title
+    margin-left 1.5rem
+
+  .address-details
+    list-style-type none
 
   .v-icon
     margin-right 1rem
--- coops-ui/tests/unit/DirectorListSm.spec.ts
+++ coops-ui/tests/unit/DirectorListSm.spec.ts
@@ -5,6 +5,7 @@ import { mount } from '@vue/test-utils'
 
 import store from '@/store/store'
 import DirectorListSm from '@/components/Dashboard/DirectorListSm.vue'
+import { EntityTypes } from '@/enums'
 
 Vue.use(Vuetify)
 Vue.use(Vuelidate)
@@ -26,8 +27,12 @@ describe('DirectorListSm', () => {
     })
   })
 
-  it('displays multiple directors', done => {
+  it('displays multiple directors when entity is a COOP', done => {
+    const wrapper = mount(DirectorListSm, { store })
+    const vm = wrapper.vm as any
+
     // init store
+    store.state.entityType = EntityTypes.Coop
     store.state.directors = [
       {
         'officer': {
@@ -57,15 +62,66 @@ describe('DirectorListSm', () => {
       }
     ]
 
+    expect(vm.directors.length).toEqual(2)
+    expect(vm.$el.querySelectorAll('.list-item').length).toEqual(2)
+
+    wrapper.destroy()
+    done()
+  })
+
+  it('displays multiple directors when entity is a BCORP', done => {
     const wrapper = mount(DirectorListSm, { store })
     const vm = wrapper.vm as any
 
-    Vue.nextTick(() => {
-      expect(vm.directors.length).toEqual(2)
-      expect(vm.$el.querySelectorAll('.list-item').length).toEqual(2)
+    // init store
+    store.state.entityType = EntityTypes.BCorp
+    store.state.directors = [
+      {
+        'officer': {
+          'firstName': 'Peter',
+          'lastName': 'Griffin'
+        },
+        'deliveryAddress': {
+          'streetAddress': '1012 Douglas St',
+          'addressCity': 'Victoria',
+          'addressRegion': 'BC',
+          'postalCode': 'V8W 2C3',
+          'addressCountry': 'CA'
+        },
+        'mailingAddress': {
+          'streetAddress': '1012 Douglas St',
+          'addressCity': 'Victoria',
+          'addressRegion': 'BC',
+          'postalCode': 'V8W 2C3',
+          'addressCountry': 'CA'
+        }
+      },
+      {
+        'officer': {
+          'firstName': 'Joe',
+          'lastName': 'Swanson'
+        },
+        'deliveryAddress': {
+          'streetAddress': '220 Buchanan St',
+          'addressCity': 'Glasgow',
+          'addressRegion': 'Scotland',
+          'postalCode': 'G1 2FFF',
+          'addressCountry': 'UK'
+        },
+        'mailingAddress': {
+          'streetAddress': '1012 Douglas St',
+          'addressCity': 'Victoria',
+          'addressRegion': 'BC',
+          'postalCode': 'V8W 2C3',
+          'addressCountry': 'CA'
+        }
+      }
+    ]
 
-      wrapper.destroy()
-      done()
-    })
+    expect(vm.directors.length).toEqual(2)
+    expect(vm.$el.querySelectorAll('.list-item').length).toEqual(4)
+
+    wrapper.destroy()
+    done()
   })
 })

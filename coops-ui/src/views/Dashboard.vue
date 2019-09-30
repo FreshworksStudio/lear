de3aea181a1f30b4205d4698bb789e653b2f914a
Cameron Bowler <cameron@freshworks.io>
2019-09-30 08:29:25 -0700

    Ongoing Office Address to include Record Office

--- coops-ui/src/views/Dashboard.vue
+++ coops-ui/src/views/Dashboard.vue
@@ -34,7 +34,16 @@
                 </v-btn>
               </header>
               <v-card flat>
-                <address-list-sm></address-list-sm>
+                <v-expansion-panel
+                :expand="true"
+                :value=0>
+                  <v-expansion-panel-content>
+                    <div slot="header">Registered Office</div>
+                    <v-card>
+                      <address-list-sm></address-list-sm>
+                    </v-card>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
               </v-card>
             </section>
 
